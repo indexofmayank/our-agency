730313164c24525e7a547257b99285a76eee7150
indexofmayank <maimayanmanu@gmail.com>
2023-11-11 23:38:21 +0530

    forrefernceforsidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import '../../styles/sidebar-styles.scss';
 
-const Sidebar = () => {
+
+function Sidebar() {
   return (
-    <div>
-      <p>mayank hoon mai</p>
-    </div>
-  )
+    <>
+      <Navbar  expand={'lg'} className="bg-body-tertiary mb-3">
+        <Container fluid>
+          <Navbar.Brand href="#">lorem brand</Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-lg`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-xl`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
+                Offcanvas
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="pe-3 nav-link-item custom-nav ">
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+                <NavDropdown
+                  title="Dropdown"
+                  id={`offcanvasNavbarDropdown-expand-lg`}
+                  className='offcanvasNavbarDropdown custom-dropdown'
+                >
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default Sidebar;
--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -1,12 +1,10 @@
 import '../styles/AppLayout.scss';
-import Sidebar from '../components/sidebar/Sidebar'; 
 import Header from '../components/header/Header';
 
 const AppLayout = () => {
     return (
     <div>
-       <Sidebar ></Sidebar>
-       <Header></Header>
+      <Header />
     </div>        
     );
 };
